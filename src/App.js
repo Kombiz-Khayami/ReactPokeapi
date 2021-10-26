a5e4642743ba58a29c1db6f0cdb70a8a760c4de9
Kombiz Khayami <kombizkhayami@outlook.com>
2021-10-26 18:28:28 -0400

    add search box functionality and ability to search by name of pokemon to get their images. still need to work on speed and smoothness though

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ let baseURL = "https://pokeapi.co/api/v2/pokemon/12";
 function App() {
   const axios = require('axios').default;
   const [post, setPost] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -17,14 +18,57 @@ function App() {
 
   if (!post) return null;
 
+  function handleSearch() {
+    baseURL = "https://pokeapi.co/api/v2/pokemon/" + searchQuery;
+    console.log(baseURL);
+    axios.get(baseURL).then((response)=> {
+      setPost(response.data);
+    });
+  }
+
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-      <p>{post.name}</p>
-      {/* <p>{post}</p>
-      <p>{post}</p> */}
+        
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Search Pokemon"
+            name="s" 
+            onChange={handleChange}
+        />
+        <button onClick={handleSearch}>Search</button>
+    {typeof post}
+        {/* {Object.entries(post).map((val) => {
+          if(typeof(val) === "object"){
+            for (const [key, value] of Object.entries(val)) {
+              return <div> {key} : {value} </div>
+            }
+          }
+          else if (typeof(val) === "array"){
+            for (const [key, value] of val) {
+              return <div> {key} : {value} </div>
+            }
+          }
+          else
+            return <div key={val.name}>{val.name}</div>
+        })} */}
+
+
+      {Object.values(post.sprites).map(val => {
+        if (typeof val === "object") {
+          return <p> {typeof val} </p>
+        }
+        else
+          return <p><img src={val} /></p>
+      })}
+
     </div>
       </header>
     </div>
