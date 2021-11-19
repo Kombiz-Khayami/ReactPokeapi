0e79bec76658e76279f03a6b77c471533a45f35c
Kombiz Khayami <kombizkhayami@outlook.com>
2021-11-19 11:50:48 -0500

    changed table to only show what's effective aginst the pokemon. also added css to make  the effectiveness clear

--- src/MakeTypeEffectivenessTable.js
+++ src/MakeTypeEffectivenessTable.js
@@ -22,144 +22,146 @@ function MakeTypeEffectivenessTable(props){
     */
 let allTypes ={
     grass: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     poison: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     bug: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     flying: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     fairy: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     ghost: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     fire: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     fighting: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     normal: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     ground: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     psychic: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     dragon: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     dark: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     steel: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     electric: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     water: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     ice: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     },
     rock: {
-        damage_dealt:1,
-        damage_taken:1
+        name:"",
+        damage_taken:100
     }
 }
     props.types.map(value => {
-        pokemonTypes[value.type.name.toLowerCase()].damage_relations.double_damage_to.map(   //grab type details based on what type the pokemon is
+        pokemonTypes[value.type.name.toLowerCase()].damage_relations.double_damage_from.map(   //grab type details based on what type the pokemon is
             val => {
-                allTypes[val.name].damage_dealt *=2;
+                allTypes[val.name].damage_taken *=2;
+                allTypes[val.name].name = "cell-"+allTypes[val.name].damage_taken;
         });
-        pokemonTypes[value.type.name.toLowerCase()].damage_relations.half_damage_to.map(     //grab type details based on what type the pokemon is
+        pokemonTypes[value.type.name.toLowerCase()].damage_relations.half_damage_from.map(     //grab type details based on what type the pokemon is
             val => {
-                allTypes[val.name].damage_dealt *=0.5;
+                allTypes[val.name].damage_taken *=0.5;
+                allTypes[val.name].name = "cell-"+allTypes[val.name].damage_taken;
+        });
+        pokemonTypes[value.type.name.toLowerCase()].damage_relations.no_damage_from.map(     //grab type details based on what type the pokemon is
+            val => {
+                allTypes[val.name].damage_taken *=0;
+                allTypes[val.name].name = "cell-"+allTypes[val.name].damage_taken;
         });
     })
 
-      return(<>
-          
-        <table>
-            <tbody>
-                <tr>
-                    <td>Nor</td>
-                    <td>Fir</td>
-                    <td>Wat</td>
-                    <td>Ele</td>
-                    <td>Gra</td>
-                    <td>Ice</td>
-                    <td>Fig</td>
-                    <td>Poi</td>
-                    <td>Gro</td>
-                </tr>
-                <tr>
-                    <td>{allTypes.normal.damage_dealt}</td>
-                    <td>{allTypes.fire.damage_dealt}</td>
-                    <td>{allTypes.water.damage_dealt}</td>
-                    <td>{allTypes.fighting.damage_dealt}</td>
-                    <td>{allTypes.electric.damage_dealt}</td>
-                    <td>{allTypes.grass.damage_dealt}</td>
-                    <td>{allTypes.ice.damage_dealt}</td>
-                    <td>{allTypes.poison.damage_dealt}</td>
-                    <td>{allTypes.ground.damage_dealt}</td>
-                </tr>
-            </tbody>
-        </table> 
 
-        <table>
-            <tbody>
-                <tr>
-                    <td>Fly</td>
-                    <td>Psy</td>
-                    <td>Bug</td>
-                    <td>Roc</td>
-                    <td>Gho</td>
-                    <td>Dra</td>
-                    <td>Dar</td>
-                    <td>Ste</td>
-                    <td>Fai</td>
-                </tr>
-                <tr>
-                    <td>{allTypes.flying.damage_dealt}</td>
-                    <td>{allTypes.psychic.damage_dealt}</td>
-                    <td>{allTypes.bug.damage_dealt}</td>
-                    <td>{allTypes.rock.damage_dealt}</td>
-                    <td>{allTypes.ghost.damage_dealt}</td>
-                    <td>{allTypes.dragon.damage_dealt}</td>
-                    <td>{allTypes.ghost.damage_dealt}</td>
-                    <td>{allTypes.steel.damage_dealt}</td>
-                    <td>{allTypes.fairy.damage_dealt}</td>                   
-                </tr>
-            </tbody>
-        </table> 
+
+      return(<>
+        <div class="type-tables">
+            <th>Damage Taken</th>
+            <table>
+                <tbody>
+                    <tr>
+                        <td class ="type-icon type-normal">Nor</td>
+                        <td class ="type-icon type-fire">Fir</td>
+                        <td class ="type-icon type-water">Wat</td>
+                        <td class ="type-icon type-electric">Ele</td>
+                        <td class ="type-icon type-grass">Gra</td>
+                        <td class ="type-icon type-ice">Ice</td>
+                        <td class ="type-icon type-fighting">Fig</td>
+                        <td class ="type-icon type-poison">Poi</td>
+                        <td class ="type-icon type-ground">Gro</td>
+                        <td class ="type-icon type-fly">Fly</td>
+                        <td class ="type-icon type-psychic">Psy</td>
+                        <td class ="type-icon type-bug">Bug</td>
+                        <td class ="type-icon type-rock">Roc</td>
+                        <td class ="type-icon type-ghost">Gho</td>
+                        <td class ="type-icon type-dragon">Dra</td>
+                        <td class ="type-icon type-dark">Dar</td>
+                        <td class ="type-icon type-steel">Ste</td>
+                        <td class ="type-icon type-fairy">Fai</td>
+                    </tr>
+                    <tr>
+                        <td class={allTypes.normal.name}>{allTypes.normal.damage_taken/100}</td>
+                        <td class={allTypes.fire.name}>{allTypes.fire.damage_taken/100}</td>
+                        <td class={allTypes.water.name}>{allTypes.water.damage_taken/100}</td>
+                        <td class={allTypes.electric.name}>{allTypes.electric.damage_taken/100}</td>
+                        <td class={allTypes.grass.name}>{allTypes.grass.damage_taken/100}</td>
+                        <td class={allTypes.ice.name}>{allTypes.ice.damage_taken/100}</td>
+                        <td class={allTypes.fighting.name}>{allTypes.fighting.damage_taken/100}</td>
+                        <td class={allTypes.poison.name}>{allTypes.poison.damage_taken/100}</td>
+                        <td class={allTypes.ground.name} >{allTypes.ground.damage_taken/100}</td>
+                        <td class={allTypes.flying.name}>{allTypes.flying.damage_taken/100}</td>
+                        <td class={allTypes.psychic.name}>{allTypes.psychic.damage_taken/100}</td>
+                        <td class={allTypes.bug.name}>{allTypes.bug.damage_taken/100}</td>
+                        <td class={allTypes.rock.name}>{allTypes.rock.damage_taken/100}</td>
+                        <td class={allTypes.ghost.name}>{allTypes.ghost.damage_taken/100}</td>
+                        <td class={allTypes.dragon.name}>{allTypes.dragon.damage_taken/100}</td>
+                        <td class={allTypes.dark.name}>{allTypes.dark.damage_taken/100}</td>
+                        <td class={allTypes.steel.name}>{allTypes.steel.damage_taken/100}</td>
+                        <td class={allTypes.fairy.name}>{allTypes.fairy.damage_taken/100}</td>                   
+                    </tr>
+                </tbody>
+            </table> 
+        </div>
       </>);
 
 } 
